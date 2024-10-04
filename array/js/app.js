c118a2d14720e7a6fe99c762f61929f12d083486
Jose Granados <josegranados99@hotmail.com>
2024-10-03 20:21:42 -0500

    list items

--- array/js/app.js
+++ array/js/app.js
@@ -1,16 +1,41 @@
 var items = localStorage.getItem('itemList')
-
 items = items ? items.split(",") : [];
 
-const adddItem = () => {
+showItems();
+
+
+const showItems = () => {
+  document.getElementById("nameItem").value = "";
+
+  let html = ""; 
+  let positionItem = 0;
+
+
+  items.forEach(item => {
+    html += `<div class='col-6 m-2'> ${item} </div>`
+    html += `<div class="col-2 m-2"> <input type="button" class="btn btn-danger" onclick="deleteItem(${positionItem})" value="X"> </div>`
+    positionItem++;
+  });
+
+  document.getElementById("listItems").innerHTML = html 
+  localStorage.setItem('itemsList', items);
+}
+
+
+const addItem = () => {
   let item = document.getElementById("nameItem").value;
+ 
   console.log("Item:", item);
 
-  if(item){
+  if (item) {
     items.push(item);
-    localStorage.setItem('itemsList', items)
+    showItems();
   }
 
   console.log("Items: ", items);
-  
+
+}
+
+const deleteItem = (item) => {
+  showItems();
 }
